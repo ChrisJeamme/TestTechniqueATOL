2e693b607f83b862932c6d3a53e5ed011dc94f7e
Chris Jeamme <chrisjahim@free.fr>
2021-01-29 20:16:32 +0100

    feat: add product use the new component

--- Application/src/app/pages/add-product/add-product.component.ts
+++ Application/src/app/pages/add-product/add-product.component.ts
@@ -1,51 +1,50 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
-import { ThemePalette } from '@angular/material/core';
 import { Router } from '@angular/router';
 import { TProduct } from 'src/app/@types/product.type';
 import { ProductService } from 'src/app/services/product.service';
 import { ServerCommunicationService } from 'src/app/services/server-communication.service';
 
 @Component({
-  selector: 'app-add-product',
-  templateUrl: './add-product.component.html',
-  styleUrls: ['./add-product.component.scss'],
+    selector: 'app-add-product',
+    templateUrl: './add-product.component.html',
+    styleUrls: ['./add-product.component.scss']
 })
-export class AddProductComponent implements OnInit {
-  newProduct: TProduct;
+export class AddProductComponent {
+    newProduct: TProduct;
 
-  constructor(
-    private serverCommunicationService: ServerCommunicationService,
-    private router: Router,
-    private productService: ProductService,
-  ) {
-    this.newProduct = {
-      name: 'AC1 Phone1',
-      type: 'phone',
-      price: 200.05,
-      rating: 3.8,
-      warrantyYears: 1,
-      available: true,
-    };
-  }
-
-  ngOnInit(): void {}
-
-  addProduct() {
-    if (this.productService.isProductValid(this.newProduct)) {
-      this.serverCommunicationService
-        .addProduct(this.newProduct as TProduct)
-        .subscribe(
-          (result) => {
-            console.log(result);
-            this.router.navigateByUrl('/');
-          },
-          (err) => {
-            console.log('Error : ', err);
-          }
-        );
-    } else {
-      console.log("Impossible d'envoyer le produit car il n'est pas valide");
+    constructor(
+        private serverCommunicationService: ServerCommunicationService,
+        private router: Router,
+        private productService: ProductService
+    ) {
+        this.newProduct = {
+            name: 'AC1 Phone1',
+            type: 'phone',
+            price: 155.5,
+            rating: 2.3,
+            warrantyYears: 2,
+            available: true
+        };
     }
-  }
+
+    addProduct(product: string) {
+        const newProduct = JSON.parse(product);
+        if (this.productService.isProductValid(newProduct)) {
+            this.serverCommunicationService
+                .addProduct(newProduct as TProduct)
+                .subscribe(
+                    (result) => {
+                        console.log(result);
+                        this.router.navigateByUrl('/');
+                    },
+                    (err) => {
+                        console.log('Error : ', err);
+                    }
+                );
+        } else {
+            console.log(
+                "Impossible d'envoyer le produit car il n'est pas valide"
+            );
+        }
+    }
 }
