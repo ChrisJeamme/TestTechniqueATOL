75f6c7f9b2d26cca2b95663303165d6626153484
Chris Jeamme <chrisjahim@free.fr>
2021-01-29 01:31:10 +0100

    fix: patch and put

--- Server/src/routes/product.ts
+++ Server/src/routes/product.ts
@@ -1,4 +1,8 @@
-import { createProduct, modifyProduct } from './../database/product'
+import {
+    createProduct,
+    modifyProduct,
+    replaceProduct
+} from './../database/product'
 import { productParamVerification } from '../functions/product.functions'
 import { TProduct } from './../@types/product.type'
 import { TProducts } from './../@types/products.type'
@@ -13,6 +17,7 @@ router.get('/:id', (req, res) => {
     const id = req.params.id as string
     getProduct(id)
         .then((product: TProduct) => {
+            console.log('Success')
             res.status(200)
                 .json({
                     product: product
@@ -35,6 +40,7 @@ router.get('/', (_req, res) => {
     console.log('API CALL - Get all products')
     getAllProducts()
         .then((list: TProducts) => {
+            console.log('Success')
             res.status(200)
                 .json({
                     products: list
@@ -47,22 +53,6 @@ router.get('/', (_req, res) => {
                     message: 'Product list get error : ' + err.message
                 })
                 .end()
-        })
-})
-
-router.delete('/:id', (req, res) => {
-    console.log('API CALL - Delete a product')
-    const id = req.params.id as string
-    deleteProduct(id)
-        .then((_result) => {
-            return res.sendStatus(200)
-        })
-        .catch((err) => {
-            if (err instanceof NotFoundError) {
-                return res.sendStatus(404)
-            } else {
-                return res.sendStatus(400)
-            }
         })
 })
 
@@ -95,6 +85,7 @@ router.post('/', (req, res) => {
 
     createProduct(productData)
         .then((_: any) => {
+            console.log('Success')
             return res.sendStatus(201)
         })
         .catch((err: Error) => {
@@ -107,21 +98,82 @@ router.post('/', (req, res) => {
 router.patch('/:id', (req, res) => {
     console.log('API CALL - Update a product')
     const id = req.params.id as string
-    const op = req.params.op as string
-    const field = req.params.field as string
-    const value = req.query[field] as string
+    const field = Object.keys(req.body)[0] as string
+    const value = req.body[field] as string
+    if (field === undefined) {
+        return res.sendStatus(422)
+    }
     if (value === undefined) {
         return res.sendStatus(422)
     }
 
     modifyProduct(id, field, value)
         .then((_: any) => {
+            console.log('Success')
             return res.sendStatus(200)
         })
         .catch((err: Error) => {
             res.status(400).json({
                 message: 'Add product error : ' + err.message
             })
+        })
+})
+
+router.put('/:id', (req, res) => {
+    console.log('API CALL - Replace a product')
+    const id = req.params.id as string
+    try {
+        productParamVerification(req.body)
+    } catch (err) {
+        if (err instanceof InvalidParameterError) {
+            return res.status(422).send({
+                message: 'Validation error in the request',
+                error: {
+                    message: 'The field is undefined',
+                    field: err.message
+                }
+            })
+        } else {
+            return res.sendStatus(400)
+        }
+    }
+
+    const productData = {
+        id: id,
+        name: req.body.name,
+        type: req.body.type,
+        price: req.body.price,
+        rating: req.body.rating,
+        warrantyYears: req.body.warranty_years,
+        available: req.body.available
+    }
+
+    replaceProduct(productData)
+        .then((_: any) => {
+            console.log('Success')
+            return res.sendStatus(201)
+        })
+        .catch((err: Error) => {
+            res.status(400).json({
+                message: 'Put product error : ' + err.message
+            })
+        })
+})
+
+router.delete('/:id', (req, res) => {
+    console.log('API CALL - Delete a product')
+    const id = req.params.id as string
+    deleteProduct(id)
+        .then((_result) => {
+            console.log('Success')
+            return res.sendStatus(200)
+        })
+        .catch((err) => {
+            if (err instanceof NotFoundError) {
+                return res.sendStatus(404)
+            } else {
+                return res.sendStatus(400)
+            }
         })
 })
 
