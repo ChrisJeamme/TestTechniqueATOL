3f6cdbda830e2c952fc039d6e1db4cedf71a5b63
Chris Jeamme <chrisjahim@free.fr>
2021-01-29 20:20:57 +0100

    fix: eslint problems

--- Application/src/app/app.module.ts
+++ Application/src/app/app.module.ts
@@ -1,6 +1,3 @@
-import { ProductService } from './services/product.service';
-import { ServerCommunicationService } from './services/server-communication.service';
-// import { StorageService } from './services/storage.service';
 import { ProductFormComponent } from './components/product-form/product-form.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatInputModule } from '@angular/material/input';
