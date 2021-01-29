b422670566d81dd349791cb2010c0a88b5e94217
Chris Jeamme <chrisjahim@free.fr>
2021-01-29 21:16:43 +0100

    feat: add jwt to the server

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -4,6 +4,8 @@ import productRoute from './routes/product'
 import connectDatabase from './database/connect'
 import { logger } from './utils/log.utils'
 import cors from 'cors'
+import jwt from 'express-jwt'
+var jwtGen = require('jsonwebtoken')
 
 // Environnement variable management
 require('dotenv').config()
@@ -18,7 +20,33 @@ app.use(bodyParser.json({}))
 // Automatically allow cross-origin requests
 app.use(cors({ origin: true }))
 
-// API Route
+// API Routing
+
+// Auth parameters
+app.use(
+    jwt({
+        secret: process.env.JWT_SECRET as string,
+        algorithms: ['HS256']
+    }).unless({ path: ['/token'] })
+)
+
+// Auth error management
+app.use((err: any, _req: any, res: any, next: Function) => {
+    if (err.name === 'UnauthorizedError') {
+        res.status(401)
+        logger.error(err.name + ': ' + err.message)
+        res.json({ message: err.name + ': ' + err.message })
+    } else next(err)
+})
+
+// Token generation
+app.get('/token', (_req, res) => {
+    res.status(200).json({
+        token: jwtGen.sign({ foo: 'bar' }, process.env.JWT_SECRET)
+    })
+})
+
+// Products routes
 app.use('/products', productRoute)
 
 // DB Connection
