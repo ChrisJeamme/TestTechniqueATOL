fd656b7068642f9d18b68b99dbf2c1af5e49e812
Chris Jeamme <chrisjahim@free.fr>
2021-01-29 02:28:58 +0100

    fix: cors

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -3,17 +3,25 @@ import bodyParser from 'body-parser'
 import productRoute from './routes/product'
 import connectDatabase from './database/connect'
 import { logger } from './utils/log.utils'
+import cors from 'cors'
 
+// Environnement variable management
 require('dotenv').config()
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
+// Enable the middleware parser for requests
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json({}))
 
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }))
+
+// API Route
 app.use('/products', productRoute)
 
+// DB Connection
 connectDatabase().then(() => {
     app.listen(PORT, () => {
         logger.info(`Server is running at https://localhost:${PORT}`)
