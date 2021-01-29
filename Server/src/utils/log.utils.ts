2a4c6296ae123202be978e0e3b6b8882ff0009ab
Chris Jeamme <chrisjahim@free.fr>
2021-01-29 21:47:57 +0100

    clean: log utils file

--- Server/src/utils/log.utils.ts
+++ Server/src/utils/log.utils.ts
@@ -6,11 +6,6 @@ export const logger = pino({
         translateTime: 'yyyy-mm-dd HH:MM:ss.l',
         ignore: 'pid,hostname'
     },
-    // level: 60, // Print FATAL
-    // level: 50, // Print ERROR & FATAL
-    // level: 40, // Print WARN & ERROR & FATAL
-    // level: 30, // Print INFO & WARN & ERROR & FATAL
-    // level: 20, // Print DEBUG & INFO & WARN & ERROR & FATAL
     level: 'trace', // Print TRACE & DEBUG & INFO & WARN & ERROR & FATAL
     prettifier: require('pino-pretty')
 })
